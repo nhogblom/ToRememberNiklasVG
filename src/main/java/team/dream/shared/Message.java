1719e3c867e3876911f1803a95a9c0a0f847b6fe
Niklas Högblom <nhogblom@gmail.com>
2026-01-11 10:45:44 +0100

    Added variable freetext, used to display a certain message that is generated by the server.

--- src/main/java/team/dream/shared/Message.java
+++ src/main/java/team/dream/shared/Message.java
@@ -9,11 +9,18 @@ public class Message implements Serializable {
     private final MessageType type;
     private final Object data;
     private String username;
+    private String freetext;
 
     public Message(MessageType type, Object data, String username) {
         this.type = type;
         this.data = data;
         this.username = username;
+    }
+    public Message(MessageType type, Object data, String username, String freetext) {
+        this.type = type;
+        this.data = data;
+        this.username = username;
+        this.freetext = freetext;
     }
 
     public Message(MessageType type, Object data) {
